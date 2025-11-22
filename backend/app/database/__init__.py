c17206289f61ed41e185c545f9d85ca9dde65f87
night9a <nightxcros@gmail.com>
2025-11-22 18:07:59 +0100

    fresh backend

--- backend/app/database/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/database/__init__.py
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-
-db = SQLAlchemy()
-migrate = Migrate()
--- backend/app/modules/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/modules/auth/__init__.py
-from flask import Blueprint
-
-
-auth_bp = Blueprint("auth", __name__)
-
-
-# import views to register routes
-from . import routes # noqa: F401
